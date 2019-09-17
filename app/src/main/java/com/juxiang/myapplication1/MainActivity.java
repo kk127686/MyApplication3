fbebf3d2736a1d86604ab68a41767d4910ba9747
kk127686 <46560445+kk127686@users.noreply.github.com>
2019-09-17 16:34:48 +0800

    更新

--- app/src/main/java/com/juxiang/myapplication1/MainActivity.java
+++ app/src/main/java/com/juxiang/myapplication1/MainActivity.java
@@ -1,36 +1,14 @@
 package com.juxiang.myapplication1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.juxiang.myapplication1.wiget.CircleView;
+import android.support.v7.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private CircleView circleView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        circleView = findViewById(R.id.circle);
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                float deg=0;
-                while(deg<360){
-                    circleView.setDeg(deg);
-                    deg+=5;
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();
 
     }
 }
